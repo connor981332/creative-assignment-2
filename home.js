9cf11c7812071b9d3dcd995db422af25a93e645e
connor981332 <connorbunch@outlook.com>
2018-10-05 09:53:20 -0600

    code cleanup

--- home.js
+++ home.js
@@ -32,26 +32,6 @@ $(document).ready(function() {
           success: function(parsed_json) {
             console.log(parsed_json);
             //TODO: Work with returned data here
-
-            /*var location = parsed_json['name'];
-            var weather = parsed_json['weather'][0]['main'];
-            var temp = parsed_json['main']['temp'];
-            var weather_icon = parsed_json['weather'][0]['icon'];
-            var humidity = parsed_json['main']['humidity'];
-            var temp_min = parsed_json['main']['temp_min'];
-            var temp_max = parsed_json['main']['temp_max'];
-            var wind_speed = parsed_json['wind']['speed'];
-            everything = "<img src=\"http://openweathermap.org/img/w/" + weather_icon + ".png\"/>"
-            everything += "<ul>";
-            everything += "<li>Location: " + location;
-            everything += "<li>Weather: " + weather + " (" + temp + "&#8457;)";
-            everything += "<li>Low: " + temp_min + "&#8457; | High: " + temp_max + "&#8457;";
-            everything += "<li>Humidity: " + humidity + "%";
-            everything += "<li>Wind: " + wind_speed + " mph";
-            everything += "</ul>";
-            $("#weather").html(everything);
-            console.log("name=" + location);
-            console.log("temp=" + temp);*/
           }
         });
       }
